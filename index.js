5c5daa626141c2560463d681f534b7badd121542
hamlyPov <tinhamly@gmail.com>
2016-11-30 14:48:46 +0700

    copy

--- index.js
+++ index.js
@@ -6,36 +6,7 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
-//var mongodb = require("mongodb");
 
-//var db;
-// Connect to the database before starting the application server.
-/*mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;*/
-  console.log("Database connection ready");
-		app.post('/webhook', function (req, res) {
-		    var events = req.body.entry[0].messaging;
-		    for (i = 0; i < events.length; i++) {
-		        var event = events[i];
-		        if (event.message && event.message.text) {
-                     
-        		    if (!kittenMessage(event.sender.id, event.message.text)) {
-        		    	
-        		        sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        		    }
-
-        		}
-		    }
-		    res.sendStatus(200);
-		});
-
-//});
 // Server frontpage
 app.get('/', function (req, res) {
     res.send('This is TestBot Server');
@@ -48,6 +19,22 @@ app.get('/webhook', function (req, res) {
     } else {
         res.send('Invalid verify token');
     }
+});
+
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            if (!kittenMessage(event.sender.id, event.message.text)) {
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            }
+        } else if (event.postback) {
+            console.log("Postback received: " + JSON.stringify(event.postback));
+        }
+    }
+    res.sendStatus(200);
 });
 
 // generic function sending messages
@@ -72,45 +59,42 @@ function sendMessage(recipientId, message) {
 // send rich message with kitten
 function kittenMessage(recipientId, text) {
     
-    // text = text || "";
-    // var values = text.split(' ');
+    text = text || "";
+    var values = text.split(' ');
     
-  /*  if (values.length === 3 && values[0] === 'kitten') {
-        if (Number(values[1]) > 0 && Number(values[2]) > 0) {*/
-        if(text){
-            if(text==='hi' || text==='hello' || text=='Hi' || text==='Hello'){
-            //var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-                var imageUrl="https://www.sefa.nl/wp-content/uploads/2016/04/koffieee.jpg";
-               
-        		   message = {
-                            "attachment": {
-                                "type": "template",
-                                "payload": {
-                                    "template_type": "generic",
-                                    "elements": [{
-                                        "title": "Kitten",
-                                        "subtitle": "Cute kitten picture",
-                                        "image_url": imageUrl ,
-                                        "buttons": [{
-                                            "type": "web_url",
-                                            "url": imageUrl,
-                                            "title": "Show kitten"
-                                            }, {
-                                            "type": "postback",
-                                            "title": "I like this",
-                                            "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                                        }]
-                                    }]
-                                }
-                            }
-                        };
-    			
-        
-                sendMessage(recipientId, message);
-                
-                return true;
-            }
+    if (values.length === 3 && values[0] === 'kitten') {
+        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+            
+            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
+            
+            message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Kitten",
+                            "subtitle": "Cute kitten picture",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show kitten"
+                                }, {
+                                "type": "postback",
+                                "title": "I like this",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        }]
+                    }
+                }
+            };
+    
+            sendMessage(recipientId, message);
+            
+            return true;
         }
+    }
     
     return false;
     
