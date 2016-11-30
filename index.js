1d2a11dc1276c6a22ba01d2cd8455612fd991581
hamlyPov <tinhamly@gmail.com>
2016-11-30 19:17:57 +0700

    db agia

--- index.js
+++ index.js
@@ -38,14 +38,25 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            if (!kittenMessage(event.sender.id, event.message.text)) {
+            db.collection(items).findOne({ title:'hamly' }, function(err, doc) {
+                if (err) {
+                  handleError(res, err.message, "Failed to get contact");
+                } else {
+                  //res.status(200).json(doc);
+                  if (!kittenMessage(event.sender.id, event.message.text)) {
+                        sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+                  }
+                  res.sendStatus(200);
+                }
+              });
+            /*if (!kittenMessage(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-            }
+            }*/
         } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
         }
     }
-    res.sendStatus(200);
+    // res.sendStatus(200);
 });
 
 // generic function sending messages
